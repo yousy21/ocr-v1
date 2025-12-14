bdcaea2c713d7a3c529fe4346b9fdc5acd941ec8
yousra's projects <yousra.ayn@gmail.com>
2025-12-14 11:56:39 +0100

    edit 5

--- components/CameraCapture.tsx
+++ components/CameraCapture.tsx
@@ -1,7 +1,11 @@
 "use client";
 import { useEffect, useRef } from "react";
 
-export default function CameraCapture() {
+interface CameraCaptureProps {
+    onCapture: (file: File) => void;
+}
+
+export default function CameraCapture({ onCapture }: CameraCaptureProps) {
     const videoRef = useRef<HTMLVideoElement>(null);
 
     useEffect(() => {
@@ -28,12 +32,38 @@ export default function CameraCapture() {
         startCamera();
     }, []);
 
+    const handleCapture = () => {
+        if (videoRef.current) {
+            const canvas = document.createElement("canvas");
+            canvas.width = videoRef.current.videoWidth;
+            canvas.height = videoRef.current.videoHeight;
+            const ctx = canvas.getContext("2d");
+            if (ctx) {
+                ctx.drawImage(videoRef.current, 0, 0);
+                canvas.toBlob((blob) => {
+                    if (blob) {
+                        const file = new File([blob], "capture.jpg", { type: "image/jpeg" });
+                        onCapture(file);
+                    }
+                }, "image/jpeg");
+            }
+        }
+    };
+
     return (
-        <video
-            ref={videoRef}
-            autoPlay
-            playsInline
-            className="w-full h-auto rounded-lg"
-        />
+        <div className="relative">
+            <video
+                ref={videoRef}
+                autoPlay
+                playsInline
+                className="w-full h-auto rounded-lg"
+            />
+            <button
+                onClick={handleCapture}
+                className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-blue-600 text-white px-4 py-2 rounded-full shadow-lg"
+            >
+                Capture
+            </button>
+        </div>
     );
 }
